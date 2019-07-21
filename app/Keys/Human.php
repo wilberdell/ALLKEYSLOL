d37de7aa9785b84b243e94634609222d307dc301
Sjors Ottjes <sjorsottjes@gmail.com>
2019-07-21 22:59:57 +0200

    randomly make users re-verify that they are not a robot

--- app/Keys/Human.php
+++ app/Keys/Human.php
@@ -6,12 +6,24 @@ class Human
 {
     public function isReal()
     {
+        // Randomly make users re-verify that they are not a robot
+        if (mt_rand(0, 200) === 42) {
+            $this->unsetCurrentUser();
+        }
+
         return (bool) session()->get('is-human');
     }
 
     public function verifyCurrentUser()
     {
         session()->put('is-human', true);
+    }
+
+    public function unsetCurrentUser()
+    {
+        session()->remove('is-human');
+
+        session()->remove('human-redirect');
     }
 
     public function putRedirectUrl($url)
