e610741672bb3f9019aab1aeb22e22d9623e8f21
Sjors Ottjes <sjorsottjes@gmail.com>
2019-08-23 10:36:37 +0200

    attempt 3

--- resources/views/components/ads/key-page-top.blade.php
+++ resources/views/components/ads/key-page-top.blade.php
@@ -1,17 +1,14 @@
 @if(App::environment('production'))
-    <div style="height:100px" class="mb-4">
-        <ins class="adsbygoogle"
-             style="display:block;"
-             data-ad-client="ca-pub-8027891891391991"
-             data-ad-slot="6714715571"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
+    <ins class="adsbygoogle mb-4"
+         style="display:inline-block;height:120px"
+         data-ad-client="ca-pub-8027891891391991"
+         data-ad-slot="6714715571"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 @else
-    <div class="mb-4" style="height: 100px; border:1px solid black;">
+    <div class="mb-4" style="height:120px; border:1px solid black;">
         key-page-top
     </div>
 @endif
