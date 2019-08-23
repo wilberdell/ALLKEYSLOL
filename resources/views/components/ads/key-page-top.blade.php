2ce18b54d4ceba237d50c77a1a1983f67428681f
Sjors Ottjes <sjorsottjes@gmail.com>
2019-08-23 10:41:59 +0200

    attempt 4

--- resources/views/components/ads/key-page-top.blade.php
+++ resources/views/components/ads/key-page-top.blade.php
@@ -1,8 +1,9 @@
 @if(App::environment('production'))
     <ins class="adsbygoogle mb-4"
-         style="display:inline-block;height:120px"
+         style="display:block"
          data-ad-client="ca-pub-8027891891391991"
          data-ad-slot="6714715571"
+         data-ad-format="auto"
          data-full-width-responsive="true"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
