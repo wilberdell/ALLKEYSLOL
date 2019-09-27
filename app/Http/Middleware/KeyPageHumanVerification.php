59135774ee7c6c128bb38d2fca86a7ad8be55c7a
Sjors Ottjes <sjorsottjes@gmail.com>
2019-09-27 13:02:53 +0200

    disable captcha

--- app/Http/Middleware/KeyPageHumanVerification.php
+++ app/Http/Middleware/KeyPageHumanVerification.php
@@ -9,18 +9,19 @@ class KeyPageHumanVerification
 {
     public function handle($request, Closure $next, $coinType)
     {
-        if (! config('keys.recaptcha_secret_key') || ! config('keys.recaptcha_site_key')) {
-            return $next($request);
-        }
-
-        $pageNumber = $request->segment('2');
-
-        if (Human::isReal() || app()->runningUnitTests() || allow_robots($coinType, $pageNumber)) {
-            return $next($request);
-        }
-
-        Human::putRedirectUrl($request->url());
-
-        return redirect()->route('humanVerification');
+        return $next($request);
+//        if (! config('keys.recaptcha_secret_key') || ! config('keys.recaptcha_site_key')) {
+//            return $next($request);
+//        }
+//
+//        $pageNumber = $request->segment('2');
+//
+//        if (Human::isReal() || app()->runningUnitTests() || allow_robots($coinType, $pageNumber)) {
+//            return $next($request);
+//        }
+//
+//        Human::putRedirectUrl($request->url());
+//
+//        return redirect()->route('humanVerification');
     }
 }
